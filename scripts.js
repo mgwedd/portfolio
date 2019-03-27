86574f2d8b2748f53b820a80f42689c73fe7ef14
mgwedd <michael.wedd@gmail.com>
2019-03-26 21:19:07 -0400

    got typewriter to the next level

--- scripts.js
+++ scripts.js
@@ -48,9 +48,4 @@ var TxtType = function(el, toRotate, period) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
     };
