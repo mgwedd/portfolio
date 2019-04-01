c767a59c3d6d7b2b1ea29bb5d31cbb49142c2706
mgwedd <michael.wedd@gmail.com>
2019-04-01 19:31:12 -0400

    switched over to TypeIt.js, decided to go more static than injected (simpler for now), and adjusted the info architecture accordingly. Also, streamlined the CSS to fit this new structure.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const instance = new TypeIt('#intro-typewriter', {
+    speed: 25,
+    waitUntilVisible: true,
+  }).go();
+
+//   I put the text into the html file to make it more SEO friendly... 
+// but then you need brs, and that maeks formatting rigid. 
+    // .type('Hi, I’m Michael.')
+    // .pause(500)
+    // .type('I’m a full-stack web developer who combines anthropology with technology to foster empathy.')
+    // .pause(1000)
+    // .break()
+    // .type('I love programming because you can build tangible things with words like these.')
+    // .pause(500)
+    // .type('Nowhere is that more true than in the Internet of Things, which we at Leverege build every day.')
+    // .pause(500)
+    // .break()
+    // .type('When I’m not programming, I’m bicycling, camping, or cooking dinner with friends.')
+    // .pause(500)
+    // .break()
+    // .type('Check out my projects and blog.')
+    // .pause(500)
+    // .type('Reach out any time!')
