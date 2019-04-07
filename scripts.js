29245ee1af46f10a3be05a305ab3a4989436a77f
mgwedd <michael.wedd@gmail.com>
2019-04-07 19:43:46 -0400

    finalized basic styles

--- scripts.js
+++ scripts.js
@@ -2,9 +2,3 @@ const instance = new TypeIt('#intro-typewriter', {
     speed: 25,
     waitUntilVisible: true,
   }).go();
-
-// $(".projects-container").hide();
-
-// setTimeout(function () {
-//   $(".projects-container").fadeIn();
-// }, 3000);
